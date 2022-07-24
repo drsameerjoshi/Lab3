102d436165e4f3a8c3a72155fa9ffcde04327b11
drsameerjoshi <105869155+drsameerjoshi@users.noreply.github.com>
2022-07-24 21:25:42 +0530

    Add files via upload

---
+++ lab3/Main.java
@@ -0,0 +1,22 @@
+package com.gl.lab3;
+
+import java.util.Stack;
+import java.util.Scanner;
+
+
+	public class Main {
+	public static void main(String[] args) 
+	{
+				Scanner var=new Scanner(System.in);
+			String expression;
+				System.out.println("Input the expression to be checked here: ");
+			expression=var.nextLine();
+			//   The method is called here
+			BalancedBrackets balancedbracket=new BalancedBrackets();
+	boolean answer=balancedbracket.areBracketsBalanced(expression);
+	if(answer)
+			System.out.println("Voila! The brackets are balanced!: ");
+		else
+			System.out.println("Oops! The brackets are unfortunately not balanced: ");
+	}
+}
